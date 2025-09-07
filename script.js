d58c9886e8ef7d4c8717cbba09c4775033fde386
sweets0uls <75015269+Idooooo125@users.noreply.github.com>
2025-09-07 19:06:30 +0700

    script.js

--- script.js
+++ script.js
@@ -34,24 +34,24 @@ document.querySelectorAll(".nav-link").forEach((link) => {
 })
 
 // fade in animasi
-const observerOptions = {
-  threshold: 0.1,
-  rootMargin: "0px 0px -50px 0px",
-}
+// const observerOptions = {
+//   threshold: 0.1,
+//   rootMargin: "0px 0px -50px 0px",
+// }
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fade-in-up")
-    }
-  })
-}, observerOptions)
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("fade-in-up")
+//     }
+//   })
+// }, observerOptions)
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const elementsToAnimate = document.querySelectorAll(".comic-panel, .project-card, .section-header, .hero-image, .struktur")
-  elementsToAnimate.forEach((el) => observer.observe(el))
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//   const elementsToAnimate = document.querySelectorAll(".comic-panel, .project-card, .section-header, .hero-image, .struktur")
+//   elementsToAnimate.forEach((el) => observer.observe(el))
+// })
 
 // fungsi project modal/popup
 function openProject(projectId) {
